b5168ca082633d8e46b1faddb64df2598247c016
Xingyu Liu <lxy.meteor@gmail.com>
2017-04-14 14:10:19 -0400

    add function name

--- webflask/application.py
+++ webflask/application.py
@@ -20,13 +20,47 @@ def login_page():
         entries = []
         return render_template('login.html', entries=entries)
 
+
 @application.route('/choosefunction', methods=['GET'])
 def choose_function():
     return render_template('choosefunction.html')
 
+@application.route('/signup', methods=['GET'])
+def signup():
+    return render_template('signup.html')
+
+@application.route('/newdatapoint', methods=['GET'])
+def new_datapoint():
+    return render_template('newdatapoint.html')
+
+@application.route('/newlocation', methods=['GET'])
+def new_location():
+    return render_template('newlocation.html')
+
+@application.route('/pendingdatapoint', methods=['GET'])
+def pending_datapoint():
+    return render_template('pendingdatapoint.html')
+
+@application.route('/pendingaccount', methods=['GET'])
+def pending_account():
+    return render_template('pendingaccount.html')
+
+@application.route('/viewpoi', methods=['GET'])
+def view_poi():
+    return render_template('viewpoi.html')
+
+@application.route('/poidetail', methods=['GET'])
+def poi_detail():
+    return render_template('poidetail.html')
+
+@application.route('/poireport', methods=['GET'])
+def poi_report():
+    return render_template('poireport.html')
+
 @application.route('/', methods=['GET'])
 def mainpage():
     return render_template('index.html')
+
 
 # run the app.
 if __name__ == "__main__":
